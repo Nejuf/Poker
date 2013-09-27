cac82b6c7c1aa7d8c2f3ad92b8851b45f9a05979
App Academy Student <student@appacademy.io>
2013-09-27 16:00:20 -0700

    Implements card category evaluations

--- lib/Hand.rb
+++ lib/Hand.rb
@@ -24,45 +24,76 @@ class Hand
   end
 
   def category_ranking
-    0
+    return 1 if royal_flush?
+    return 2 if straight_flush?
+    return 3 if four_of_a_kind?
+    return 4 if full_house?
+    return 5 if flush?
+    return 6 if straight?
+    return 7 if three_of_a_kind?
+    return 8 if two_pair?
+    return 9 if one_pair?
+    return 10 if high_card
   end
 
   private
 
       def royal_flush?
-
+        if straight_flush?
+          return @cards.all? do |card|
+            card.number >= 10 || card.number == 1
+          end
+        end
+        false
       end
 
       def straight_flush?
-
+        flush? && straight?
       end
 
       def four_of_a_kind?
-
+        num_card_occurrences.has_value?(4)
       end
 
       def full_house?
+        three_of_a_kind? && one_pair?
+      end
 
+      def flush?
+        suit = @cards[0].suit
+        @cards.all? {|card| card.suit == suit}
       end
 
       def straight?
-
+        sorted_numbers = num_card_occurrences.keys.sort
+        4.times do |index|
+          return false sorted_numbers[index+1] - sorted_numbers[index] != 1
+        end
+        true
       end
 
       def three_of_a_kind?
-
+        num_card_occurrences.has_value?(3)
       end
 
       def two_pair?
-
+        num_card_occurrences.values.find_all { |num_cards| num_cards == 2 }.length == 2
       end
 
       def one_pair?
-
+        num_card_occurrences.has_value?(2)
       end
 
       def high_card
+        @cards.sort_by(&:number).pop
+      end
 
+      def num_card_occurrences
+        hash = Hash.new(0)
+        @cards.each do |card|
+          hash[card.number] += 1
+        end
+        return hash
       end
 
 end
